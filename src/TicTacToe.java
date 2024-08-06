336d684de651db58c74f05e61c673ee9750dbacc
Justin <justinhg720@gmail.com>
2024-08-06 12:48:04 -0400

    Finalized the base of the game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -5,7 +5,10 @@ import java.awt.event.ActionListener;
 
 public class TicTacToe implements ActionListener
 {
-    JButton[] buttons = new JButton[9];
+    private JButton[] buttons = new JButton[9];
+    private boolean player1Turn = true;
+    private boolean finished = false;
+    private JLabel title;
     public TicTacToe()
     {
         JFrame frame = new JFrame();
@@ -25,8 +28,9 @@ public class TicTacToe implements ActionListener
         panel.add(titlePanel);
         panel.add(new Spacer(new Dimension(800,10)));
 
-        JLabel title = new JLabel("Tic Tac Toe");
-        title.setFont(new Font("Arial",Font.BOLD,65));
+        title = new JLabel();
+        title.setText(getFormattedText());
+        title.setFont(new Font("Arial",Font.BOLD,45));
         titlePanel.add(title);
 
         JPanel buttonPanel = new JPanel();
@@ -36,16 +40,22 @@ public class TicTacToe implements ActionListener
         JPanel innerPanel = new JPanel();
         innerPanel.setOpaque(false);
         innerPanel.setMaximumSize(new Dimension(740,450));
+        innerPanel.setLayout(new GridLayout(3,3));
         buttonPanel.add(new Spacer(new Dimension(780,20)));
         buttonPanel.add(innerPanel);
         buttonPanel.add(new Spacer(new Dimension(780,20)));
-        /*
+
         for(int i = 0; i < 9; i++)
         {
             buttons[i] = new JButton();
-            buttons[i].
+            buttons[i].setText("");
+            buttons[i].setFont(new Font("Arial", Font.BOLD,65));
+            buttons[i].setOpaque(false);
+            buttons[i].setForeground(Color.CYAN);
+            buttons[i].addActionListener(this);
+            innerPanel.add(buttons[i]);
         }
-        */
+
         panel.add(buttonPanel);
         panel.add(new Spacer(new Dimension(800,10)));
 
@@ -56,6 +66,112 @@ public class TicTacToe implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        for(int i = 0; i < 9; i++)
+        {
+            if(e.getSource() == buttons[i] && player1Turn && buttons[i].getText().isEmpty() && !finished)
+            {
+                buttons[i].setForeground(new Color(209, 0, 0));
+                buttons[i].setText("X");
+                player1Turn = !player1Turn;
+                title.setText(getFormattedText());
+                if(!(getWinner() == null))
+                {
+                    title.setText(getWinner() + " WINS!");
+                }
+            }
+            else if(e.getSource() == buttons[i] && !player1Turn && buttons[i].getText().isEmpty() && !finished)
+            {
+                buttons[i].setForeground(new Color(53, 211, 0));
+                buttons[i].setText("O");
+                player1Turn = !player1Turn;
+                title.setText(getFormattedText());
+                if(!(getWinner() == null))
+                {
+                    title.setText(getWinner() + " WINS!");
+                }
+            }
+        }
+    }
 
+    public String getFormattedText()
+    {
+        if(player1Turn)
+        {
+            return "Tic Tac Toe -- Player 1's Turn";
+        }
+        else
+        {
+            return "Tic Tac Toe -- Player 2's Turn";
+        }
+    }
+
+    public String getWinner()
+    {
+
+        if(buttons[0].getText().equals(buttons[1].getText()) && buttons[1].getText().equals(buttons[2].getText()))
+        {
+            if(!(buttons[0].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[0].getText();
+            }
+        }
+        else if(buttons[3].getText().equals(buttons[4].getText()) && buttons[4].getText().equals(buttons[5].getText()))
+        {
+            if(!(buttons[3].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[3].getText();
+            }
+        }
+        else if(buttons[6].getText().equals(buttons[7].getText()) && buttons[7].getText().equals(buttons[8].getText()))
+        {
+            if(!(buttons[6].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[6].getText();
+            }
+        }
+        else if(buttons[0].getText().equals(buttons[3].getText()) && buttons[3].getText().equals(buttons[6].getText()))
+        {
+            if(!(buttons[0].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[0].getText();
+            }
+        }
+        else if(buttons[1].getText().equals(buttons[4].getText()) && buttons[4].getText().equals(buttons[7].getText()))
+        {
+            if(!(buttons[1].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[1].getText();
+            }
+        }
+        else if(buttons[2].getText().equals(buttons[5].getText()) && buttons[5].getText().equals(buttons[8].getText()))
+        {
+            if(!(buttons[2].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[2].getText();
+            }
+        }
+        else if(buttons[0].getText().equals(buttons[4].getText()) && buttons[4].getText().equals(buttons[8].getText()))
+        {
+            if(!(buttons[0].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[0].getText();
+            }
+        }
+        else if(buttons[2].getText().equals(buttons[4].getText()) && buttons[4].getText().equals(buttons[6].getText()))
+        {
+            if(!(buttons[2].getText().isEmpty()))
+            {
+                finished = true;
+                return buttons[2].getText();
+            }
+        }
+        return null;
     }
 }
